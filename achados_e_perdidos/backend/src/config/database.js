a21e9afeeb34d9c884ae945a495bf68bac3cb8c8
Guylherme <guylherme.l.e@gmail.com>
2026-06-21 15:47:05 -0300

    fix: conexão com servidor

--- achados_e_perdidos/backend/src/config/database.js
+++ achados_e_perdidos/backend/src/config/database.js
@@ -7,7 +7,8 @@ const sequelize = new Sequelize(
     process.env.DB_PASSWORD || "",
     {
         host: process.env.DB_HOST || "localhost",
-        dialect: "mysql"
+        dialect: "mysql",
+        port: process.env.DB_PORT || 3306
     }
 );
 
--- achados_e_perdidos/backend/src/server.js
+++ achados_e_perdidos/backend/src/server.js
@@ -1,5 +1,6 @@
 const app = require('./app.js');
-require('dotenv').config();
+const dotenv = require('dotenv');
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
